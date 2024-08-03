bfdbd47db611a66aa887c0de940c4042d91638c4
YuiKun2002 <1033082401@qq.com>
2024-08-03 13:16:36 +0800

    新增SpineActor

--- Plugins/SpinePlugin/Source/SpinePlugin/SpinePlugin.Build.cs
+++ Plugins/SpinePlugin/Source/SpinePlugin/SpinePlugin.Build.cs
@@ -3,20 +3,28 @@ using System.IO;
 
 namespace UnrealBuildTool.Rules
 {
-	public class SpinePlugin : ModuleRules
-	{
-		public SpinePlugin(ReadOnlyTargetRules target) : base(target)
-		{
-			PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public class SpinePlugin : ModuleRules
+    {
+        public SpinePlugin(ReadOnlyTargetRules target) : base(target)
+        {
+            PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-			PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
-			PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public/spine-cpp/include"));
+            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+            PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public/spine-cpp/include"));
 
-			PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
-			PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Public/spine-cpp/include"));
+            PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
+            PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Public/spine-cpp/include"));
 
-            PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "ProceduralMeshComponent", "UMG", "Slate", "SlateCore" });
+            PublicDependencyModuleNames.AddRange(new string[] {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "ProceduralMeshComponent",
+                "UMG",
+                "Slate",
+                "SlateCore"
+            });
             PublicDefinitions.Add("SPINE_UE4");
-		}
-	}
+        }
+    }
 }
