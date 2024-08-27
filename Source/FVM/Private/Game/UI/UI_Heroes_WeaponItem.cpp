c9ae1111346e733dfdefa3b4d33b560b1704ba01
YuiKun2002 <1033082401@qq.com>
2024-08-27 10:58:35 +0800

    完成武器道具的等级显示

--- Source/FVM/Private/Game/UI/UI_Heroes_WeaponItem.cpp
+++ Source/FVM/Private/Game/UI/UI_Heroes_WeaponItem.cpp
@@ -16,6 +16,15 @@ void UUI_Heroes_WeaponItem::SetWeaponData(const FItemWeaponBase& Data)
 
 void UUI_Heroes_WeaponItem::InitWeaponData()
 {
+	if (this->WeaponData.WeaponLevel < 0)
+	{
+		this->WeaponData.WeaponLevel = 0;
+	}
+	else if (this->WeaponData.WeaponLevel > 4)
+	{
+		this->WeaponData.WeaponLevel = 4;
+	}
+
 	//设置按钮外观
 	UWidgetBase::SetButtonStyleSoft(this->Item,
 		TSoftObjectPtr<UTexture2D>(this->WeaponData.ItemTexturePath)
@@ -24,5 +33,10 @@ void UUI_Heroes_WeaponItem::InitWeaponData()
 	ItemLevel->SetText(FText::FromString(FString::FromInt(this->WeaponData.WeaponLevel + 1)));
 	//获取缓存对象
 	UHeroItemDataAssetCache* Data = GetGameDataAssetCache<UHeroItemDataAssetCache>(HEROITEM_HEROITEM);
-	Data->GetResource(HEROITEM_ITEMLEVEL,TEXT("道具星星等级"),0);
+	FSoftObjectPath LevelPath = Data->GetResource(HEROITEM_ITEMLEVEL, TEXT("道具星星等级"), this->WeaponData.WeaponLevel);
+	//设置星级
+	UWidgetBase::SetImageBrushByTexture(
+		this->ItemStars,
+		TSoftObjectPtr<UTexture2D>(LevelPath)
+	);
 }
