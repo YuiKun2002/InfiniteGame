92cd06eb094f5b5e2f1560ff414c2aa028c7c182
YuiKun2002 <1033082401@qq.com>
2024-10-03 01:11:57 +0800

    完成主地图，新修改UI，适配UI

--- Source/FVM/Private/Game/UI/HeroesData.cpp
+++ Source/FVM/Private/Game/UI/HeroesData.cpp
@@ -2,6 +2,7 @@
 
 FSoftObjectPath UHeroItemDataAssetCache::GetResource(const FName& Name, const FName& RowName, const int32& Key)
 {
+	//如果当前缓存名称与新名称相同【数据表中的RowName】
 	if (this->ResourceName.IsEqual(Name))
 	{
 		if (this->Datas.Num())
@@ -15,12 +16,13 @@ FSoftObjectPath UHeroItemDataAssetCache::GetResource(const FName& Name, const FN
 			return FSoftObjectPath();
 		}
 	}
-	//清理数据
+
+	//如果是不同的RowName则重新初始化
 	this->Datas.Empty();
-	//初始化
+	//初始化【查询全局纹理等级资源】
 	this->Data.InitDataTable(
 		UGameDataSubsystem::GetGameDataSubsystemStatic()->GetAsset()->
-		GetDataByName(GLOBALASSET_LEVELTEXTURE_RESOURCE)->GetDataByName(Name)
+		GetDataByName(GLOBALASSET_LEVELTEXTURE_RESOURCE)->GetDataByName(Name) //获取Row
 	);
 	//获取行
 	FItemResourceData RowData = this->Data.GetDataByName(RowName);
