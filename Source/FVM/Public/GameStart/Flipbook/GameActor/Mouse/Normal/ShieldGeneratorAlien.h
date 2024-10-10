03d0a7b2f385cb0b19caa18f5a16dcb9fd87e633
YuiKun2002 <1033082401@qq.com>
2024-10-10 17:06:56 +0800

    修改完成，治疗外星人

---
+++ Source/FVM/Public/GameStart/Flipbook/GameActor/Mouse/Normal/ShieldGeneratorAlien.h
@@ -0,0 +1,17 @@
+﻿// 该游戏是同人游戏，提供学习使用，禁止贩卖，如有侵权立刻删除
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameStart/Flipbook/GameActor/Mouse/Normal/NormalBase.h"
+#include "ShieldGeneratorAlien.generated.h"
+
+/**
+ * 护盾生成
+ */
+UCLASS()
+class FVM_API AShieldGeneratorAlien : public ANormalCapsuleBase
+{
+	GENERATED_BODY()
+	
+};
