2f4b57979f7ea011baeddbcb994a96dbd9bffa9a
YuiKun2002 <1033082401@qq.com>
2024-11-11 18:44:21 +0800

    新增插件

---
+++ Plugins/XiaoYuNetURL/Source/XiaoYuNetURL/Private/XiaoYuNetURLBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "XiaoYuNetURLBPLibrary.h"
+#include "XiaoYuNetURL.h"
+
+UXiaoYuNetURLBPLibrary::UXiaoYuNetURLBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UXiaoYuNetURLBPLibrary::XiaoYuNetURLSampleFunction(float Param)
+{
+	return -1;
+}
+
