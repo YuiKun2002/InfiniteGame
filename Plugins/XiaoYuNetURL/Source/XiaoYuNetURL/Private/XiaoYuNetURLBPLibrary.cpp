da469c6f901b41e6219a5c7fcd3055707117590e
YuiKun2002 <1033082401@qq.com>
2024-11-11 19:04:00 +0800

    更新

--- Plugins/XiaoYuNetURL/Source/XiaoYuNetURL/Private/XiaoYuNetURLBPLibrary.cpp
+++ Plugins/XiaoYuNetURL/Source/XiaoYuNetURL/Private/XiaoYuNetURLBPLibrary.cpp
@@ -4,13 +4,57 @@
 #include "XiaoYuNetURL.h"
 
 UXiaoYuNetURLBPLibrary::UXiaoYuNetURLBPLibrary(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer)
+	: Super(ObjectInitializer)
 {
 
 }
 
-float UXiaoYuNetURLBPLibrary::XiaoYuNetURLSampleFunction(float Param)
+void UXiaoYuNetURLBPLibrary::XGetBaseURL(FString& URL)
 {
-	return -1;
+	URL = TEXT("http://192.168.1.8:8888/api/") + URL;
+}
+
+void UXiaoYuNetURLBPLibrary::XGetLoginURL(FString& URL, int32 State)
+{
+	switch (State)
+	{
+	case 2:
+		UXiaoYuNetURLBPLibrary::XGetBaseURL(URL + TEXT("user/register"))
+			; break;
+	case 3:
+		UXiaoYuNetURLBPLibrary::XGetBaseURL(URL + TEXT("user/logout"));
+		break;
+	case 4:
+		UXiaoYuNetURLBPLibrary::XGetBaseURL(URL + TEXT("user/update"))
+			; break;
+	default:
+		UXiaoYuNetURLBPLibrary::XGetBaseURL(URL + TEXT("user/login"));
+		break;
+	}
+}
+
+FString UXiaoYuNetURLBPLibrary::XGetPlayerCoinURL(const FString& UserID)
+{
+	FString Str;
+	UXiaoYuNetURLBPLibrary::XGetBaseURL(Str);
+
+	Str += TEXT("money/getMoney?userId=") + UserID;
+
+	return Str;
+}
+
+FString UXiaoYuNetURLBPLibrary::XGetPlayerBagURL(const FString& UserID)
+{
+	FString Str;
+	UXiaoYuNetURLBPLibrary::XGetBaseURL(Str);
+
+	Str += TEXT("package/getPackageItems?userId=") + UserID;
+
+	return Str;
+}
+
+void UXiaoYuNetURLBPLibrary::XGetShopURL(FString& URL)
+{
+	UXiaoYuNetURLBPLibrary::XGetBaseURL(URL + TEXT("item/queryAllItem"));
 }
 
--- Plugins/XiaoYuNetURL/Source/XiaoYuNetURL/Public/XiaoYuNetURLBPLibrary.h
+++ Plugins/XiaoYuNetURL/Source/XiaoYuNetURL/Public/XiaoYuNetURLBPLibrary.h
@@ -5,7 +5,7 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "XiaoYuNetURLBPLibrary.generated.h"
 
-/* 
+/*
 *	Function library class.
 *	Each function in it is expected to be static and represents blueprint node that can be called in any blueprint.
 *
@@ -15,18 +15,35 @@
 *	DisplayName - full name of the node, shown when you mouse over the node and in the blueprint drop down menu.
 *				Its lets you name the node using characters not allowed in C++ function names.
 *	CompactNodeTitle - the word(s) that appear on the node.
-*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu. 
+*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu.
 *				Good example is "Print String" node which you can find also by using keyword "log".
 *	Category -	the category your node will be under in the Blueprint drop-down menu.
 *
 *	For more info on custom blueprint nodes visit documentation:
 *	https://wiki.unrealengine.com/Custom_Blueprint_Node_Creation
 */
+
+
+
 UCLASS()
 class UXiaoYuNetURLBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
 
-	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Execute Sample function", Keywords = "XiaoYuNetURL sample test testing"), Category = "XiaoYuNetURLTesting")
-	static float XiaoYuNetURLSampleFunction(float Param);
+	//获取基本的URL
+	UFUNCTION(BlueprintPure, Category = "XiaoYuNetURLTesting")
+	static void XGetBaseURL(FString& URL);
+public:
+	//获取登录URL[1登录，2注册，3登出，4更新，5注销]
+	UFUNCTION(BlueprintPure, Category = "XiaoYuNetURLTesting|登录")
+	static void XGetLoginURL(FString& URL,int32 State);
+	//获取角色货币URL
+	UFUNCTION(BlueprintPure, Category = "XiaoYuNetURLTesting|角色")
+	static FString XGetPlayerCoinURL(const FString& UserID);
+	//获取角色背包URL
+	UFUNCTION(BlueprintPure, Category = "XiaoYuNetURLTesting|角色")
+	static FString XGetPlayerBagURL(const FString& UserID);
+	//获取商城URL
+	UFUNCTION(BlueprintPure, Category = "XiaoYuNetURLTesting|商城")
+	static void XGetShopURL(FString& URL);
 };
