9042907655c397991f02f0e2bb4f8523995dc3b9
YuiKun2002 <1033082401@qq.com>
2024-11-17 13:56:42 +0800

    修复

--- Source/FVM/Private/GameStart/Flipbook/GameActor/Mouse/Normal/CarAlien.cpp
+++ Source/FVM/Private/GameStart/Flipbook/GameActor/Mouse/Normal/CarAlien.cpp
@@ -148,135 +148,135 @@ void ACarAlien::PlayMoveAnim()
 
 
 
-ACarAlienWidget::ACarAlienWidget()
-{
-	this->CheckWidLeftLocationComp = CreateDefaultSubobject<USceneComponent>(TEXT("CheckWidLeftLocationComp"));
-	this->CheckWidLeftLocationComp = CreateDefaultSubobject<USceneComponent>(TEXT("CheckWidRightLocationComp"));
-}
-
-void ACarAlienWidget::BeginPlay()
-{
-	Super::BeginPlay();
-
-	//初始化碰撞网格位置
-	this->MesheComp->SetBoxExtent(this->BoxCompSize, true);
-	this->MesheComp->AddLocalOffset(this->CollisionOffset);
-	this->BodyComp->AddLocalOffset(this->BodyCollisionOffset);
-}
-
-void ACarAlienWidget::MouseInit()
-{
-	Super::MouseInit();
-
-	this->PlayMoveAnim();
-}
-
-void ACarAlienWidget::BeAttakedBegin()
-{
-	Super::BeAttakedBegin();
-
-	this->PlayMoveAnim();
-}
-
-void ACarAlienWidget::MoveingUpdate(float DeltaTime)
-{
-	Super::MoveingUpdate(DeltaTime);
-
-	this->UpdateMove(DeltaTime);
-
-	if (Time < 0.2f)
-	{
-		Time += DeltaTime;
-	}
-	else {
-		Time = 0.f;
-		this->CheckCard();
-	}
-}
-
-void ACarAlienWidget::MouseDeathed()
-{
-	this->ClosedBoxComponent(this->MesheComp);
-	this->BodyComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-
-	Super::MouseDeathed();
-
-	if (!this->GetPlayPlayBombEffAnim())
-	{
-		UTrackEntry* Trac = this->SetAnimation(0,
-			this->M_DefAnim_Anim.DeadAnimRes.GetDefaultObject()->GetCategoryName().ToString(), true
-		);
-		BINDANIMATION(Trac, this, &AMouseActor::AlienDeadAnimationCompelet);
-		this->SetTrackEntry(Trac);
-	}
-}
-
-void ACarAlienWidget::CheckCard()
-{
-	//获取网格
-	UMesheControllComponent* LocalMesheComp = AGameMapInstance::GetGameMapInstance()->M_MesheControllComponent;
-
-	//获取当前线路
-	int32 CurLineZ = LocalMesheComp->GetMapMesh(
-		this->GetMouseLine().Row, 0
-	)->GetActorLocation().Z;
-
-	//设置基本位置
-	FVector LocationBegin = this->GetActorLocation() + FVector(
-		this->CheckWidLeftLocationComp->GetRelativeLocation().Y,
-		this->CheckWidLeftLocationComp->GetRelativeLocation().X,
-		this->CheckWidLeftLocationComp->GetRelativeLocation().Z
-	);
-	LocationBegin.Z = CurLineZ;
-
-	FVector LocationEnd = this->GetActorLocation() + FVector(
-		this->CheckWidLeftLocationComp->GetRelativeLocation().Y,
-		this->CheckWidLeftLocationComp->GetRelativeLocation().X,
-		this->CheckWidLeftLocationComp->GetRelativeLocation().Z
-	);
-	LocationEnd.Z = CurLineZ;
-
-	//添加检测
-	UGameSystemFunction::AddLineTrance(
-		this->GetWorld(), LocationBegin, LocationEnd,
-		UGameSystemFunction::GetCardCollisionBoxType(this->CheckCardType), this,
-		[](UObject* GunAlienActor, AActor* CardActor) {
-			if (IsValid(Cast<ACardActor>(CardActor)))
-			{
-				Cast<ACardActor>(CardActor)->SetCardCurrentHP(0.f);
-				Cast<ACardActor>(CardActor)->KillCard();
-				UResourceManagerComponent::ResourceAddBadCard();
-			}
-		}
-	);
-}
-
-void ACarAlienWidget::PlayMoveAnim()
-{
-	if (this->GetCurrentHP() > this->GetTotalHP() * 0.5f)
-	{
-		if (this->State != 0)
-		{
-			//走路状态
-			this->State = 0;
-			//播放走路动画
-			UTrackEntry* Track = this->SetAnimation(0,
-				this->M_DefAnim_Anim.WalkAnimRes.GetDefaultObject()->GetCategoryName().ToString(),
-				true);
-			this->SetTrackEntry(Track);
-		}
-	}
-	else {
-		if (this->State != 1)
-		{
-			//走路状态
-			this->State = 1;
-			//播放走路动画
-			UTrackEntry* Track = this->SetAnimation(0,
-				this->M_DefAnim_Anim.WalkAnimDamageRes.GetDefaultObject()->GetCategoryName().ToString(),
-				true);
-			this->SetTrackEntry(Track);
-		}
-	}
-}
+//ACarAlienWidget::ACarAlienWidget()
+//{
+//	this->CheckWidLeftLocationComp = CreateDefaultSubobject<USceneComponent>(TEXT("CheckWidLeftLocationComp"));
+//	this->CheckWidLeftLocationComp = CreateDefaultSubobject<USceneComponent>(TEXT("CheckWidRightLocationComp"));
+//}
+//
+//void ACarAlienWidget::BeginPlay()
+//{
+//	Super::BeginPlay();
+//
+//	//初始化碰撞网格位置
+//	this->MesheComp->SetBoxExtent(this->BoxCompSize, true);
+//	this->MesheComp->AddLocalOffset(this->CollisionOffset);
+//	this->BodyComp->AddLocalOffset(this->BodyCollisionOffset);
+//}
+//
+//void ACarAlienWidget::MouseInit()
+//{
+//	Super::MouseInit();
+//
+//	this->PlayMoveAnim();
+//}
+//
+//void ACarAlienWidget::BeAttakedBegin()
+//{
+//	Super::BeAttakedBegin();
+//
+//	this->PlayMoveAnim();
+//}
+//
+//void ACarAlienWidget::MoveingUpdate(float DeltaTime)
+//{
+//	Super::MoveingUpdate(DeltaTime);
+//
+//	this->UpdateMove(DeltaTime);
+//
+//	if (Time < 0.2f)
+//	{
+//		Time += DeltaTime;
+//	}
+//	else {
+//		Time = 0.f;
+//		this->CheckCard();
+//	}
+//}
+//
+//void ACarAlienWidget::MouseDeathed()
+//{
+//	this->ClosedBoxComponent(this->MesheComp);
+//	this->BodyComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+//
+//	Super::MouseDeathed();
+//
+//	if (!this->GetPlayPlayBombEffAnim())
+//	{
+//		UTrackEntry* Trac = this->SetAnimation(0,
+//			this->M_DefAnim_Anim.DeadAnimRes.GetDefaultObject()->GetCategoryName().ToString(), true
+//		);
+//		BINDANIMATION(Trac, this, &AMouseActor::AlienDeadAnimationCompelet);
+//		this->SetTrackEntry(Trac);
+//	}
+//}
+//
+//void ACarAlienWidget::CheckCard()
+//{
+//	//获取网格
+//	UMesheControllComponent* LocalMesheComp = AGameMapInstance::GetGameMapInstance()->M_MesheControllComponent;
+//
+//	//获取当前线路
+//	int32 CurLineZ = LocalMesheComp->GetMapMesh(
+//		this->GetMouseLine().Row, 0
+//	)->GetActorLocation().Z;
+//
+//	//设置基本位置
+//	FVector LocationBegin = this->GetActorLocation() + FVector(
+//		this->CheckWidLeftLocationComp->GetRelativeLocation().Y,
+//		this->CheckWidLeftLocationComp->GetRelativeLocation().X,
+//		this->CheckWidLeftLocationComp->GetRelativeLocation().Z
+//	);
+//	LocationBegin.Z = CurLineZ;
+//
+//	FVector LocationEnd = this->GetActorLocation() + FVector(
+//		this->CheckWidLeftLocationComp->GetRelativeLocation().Y,
+//		this->CheckWidLeftLocationComp->GetRelativeLocation().X,
+//		this->CheckWidLeftLocationComp->GetRelativeLocation().Z
+//	);
+//	LocationEnd.Z = CurLineZ;
+//
+//	//添加检测
+//	UGameSystemFunction::AddLineTrance(
+//		this->GetWorld(), LocationBegin, LocationEnd,
+//		UGameSystemFunction::GetCardCollisionBoxType(this->CheckCardType), this,
+//		[](UObject* GunAlienActor, AActor* CardActor) {
+//			if (IsValid(Cast<ACardActor>(CardActor)))
+//			{
+//				Cast<ACardActor>(CardActor)->SetCardCurrentHP(0.f);
+//				Cast<ACardActor>(CardActor)->KillCard();
+//				UResourceManagerComponent::ResourceAddBadCard();
+//			}
+//		}
+//	);
+//}
+//
+//void ACarAlienWidget::PlayMoveAnim()
+//{
+//	if (this->GetCurrentHP() > this->GetTotalHP() * 0.5f)
+//	{
+//		if (this->State != 0)
+//		{
+//			//走路状态
+//			this->State = 0;
+//			//播放走路动画
+//			UTrackEntry* Track = this->SetAnimation(0,
+//				this->M_DefAnim_Anim.WalkAnimRes.GetDefaultObject()->GetCategoryName().ToString(),
+//				true);
+//			this->SetTrackEntry(Track);
+//		}
+//	}
+//	else {
+//		if (this->State != 1)
+//		{
+//			//走路状态
+//			this->State = 1;
+//			//播放走路动画
+//			UTrackEntry* Track = this->SetAnimation(0,
+//				this->M_DefAnim_Anim.WalkAnimDamageRes.GetDefaultObject()->GetCategoryName().ToString(),
+//				true);
+//			this->SetTrackEntry(Track);
+//		}
+//	}
+//}
 
